3d51a9c4c6fa7b8c3eea7079fea86c7d61e55c88
Adnan Ashraf <adnan.ashraff1@gmail.com>
2025-10-01 04:34:16 +0500

    Update checkout.js

--- pb_hooks/checkout.js
+++ pb_hooks/checkout.js
@@ -1,4 +1,6 @@
 onBeforeServe((e) => {
+  console.log("✅ Checkout route loaded");
+
   e.router.add("POST", "/checkout", async (c) => {
     return c.json(200, { success: true, message: "Checkout route working!" });
   });
